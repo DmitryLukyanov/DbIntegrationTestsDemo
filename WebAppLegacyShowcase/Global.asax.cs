5f601fb789354c2968e9210b8c4dae2f40a34c00
DmitryLukyanov <dzima.lukyanau@gmail.com>
2025-02-21 01:13:21 +0400

    draft

--- WebAppLegacyShowcase/Global.asax.cs
+++ WebAppLegacyShowcase/Global.asax.cs
@@ -18,6 +18,7 @@ namespace WebAppLegacyShowcase
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            new ProjectReferenceLegacy.ProjectReferenceLegacyWrapper();
         }
     }
 }
